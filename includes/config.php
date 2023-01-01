12801410fdb714c4e329d8e1d103ff691aa30716
Busayo Odukoya <odukoyabusayo@gmail.com>
2023-01-01 19:41:56 +0100

    added Db connect

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-    $con  = new PDO("mysql:dbname=busayoflix;host=localhost", "root", "");
+    $con  = new PDO("mysql:dbname=busayoflix;host=busayoflix.mysql.database.azure.com", "busayo", "Password12");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
